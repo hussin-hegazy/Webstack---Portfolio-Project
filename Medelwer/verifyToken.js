809deabe36d8bd5ff5b6062e0b74b2e385d1449c
hussin-hegazy <hussinhegazy350@gmail.com>
2025-01-18 15:18:18 +0200

    Improve verifyToken.js: - Ensure Authorization header contains Bearer before extracting the token. - Add clear error message for invalid or expired tokens. - Add try/catch to handle errors during token verification.

--- Medelwer/verifyToken.js
+++ Medelwer/verifyToken.js
@@ -5,17 +5,24 @@ const asyncWrapper = require('../Medelwer/asyncWrapper')
 
 const  verifyToken = asyncWrapper (async(req, res, next) => {
         const Auth_token = req.headers['Authorization']|| req.headers['authorization']
-        if(! Auth_token){
-           const error = AppError.create("'Access denied. No token provided.'", 401, 'Fail')
+        if(! Auth_token || !Auth_token.toLowerCase().startsWith("bearer ")){
+        
+           const error = AppError.create("'Access denied. No token provided'", 401, 'Fail')
            return next(error)
         }
-        const token = Auth_token.split(" ")[1]
+
+       const token = Auth_token.split(" ")[1]
+
+       try {
         
         const decoded = jwt.verify(token, process.env.Secret_K_JWT)
         req.user_data = decoded
-        console.log(decoded)
          next()
-
+      }
+      catch(err){
+         const error = AppError.create("Invalid or expired token", 401, 'Fail')
+         next(error)
+      }
 }
 )
 
