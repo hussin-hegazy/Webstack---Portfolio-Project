49055eb62eab1e6cb701cdc64d5b69413bbbd61b
hussin-hegazy <hussinhegazy350@gmail.com>
2025-01-18 16:01:47 +0200

    Add error message for access denial and ensure user has allowed roles

--- Medelwer/verifyRole.js
+++ Medelwer/verifyRole.js
@@ -1,8 +1,13 @@
+const AppError = require('../utils/AppError')
+
 
 module.exports = (...roles) => {
     return (req, res, next) => {
+
        if(!roles.includes(req.user_data.role)){
-        return res.status(403).send('Access denied')
+        
+        return next(AppError.create("Access denied, You dont have permissionto access this resource", 403, "Fail"))
+
        }
     next()
     }
