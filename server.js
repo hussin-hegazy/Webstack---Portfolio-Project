0562b09f0b4448ed71235689cf4a45d39d923e11
hussin-hegazy <hussinhegazy350@gmail.com>
2025-01-19 02:56:23 +0200

    Configured passport and added user routes for Google login

--- server.js
+++ server.js
@@ -2,13 +2,20 @@ const mongoose = require('mongoose');
 
 const express = require("express");
 
+const passport = require('passport')
+
 const path = require('path')
 
 require('dotenv').config()
 
+require('./config/passport')
+
+
 const task_Routes = require('./routes/taskRoutes')
 
 const user_Routes = require('./routes/userRoutes')
+
+require('./config/passport')
 
 
 const app = express()
@@ -32,6 +39,7 @@ app.use('/api/users', user_Routes)
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
+app.use(passport.initialize())
 
 // Hindeler Globel Error
 app.use((err, req, res, next) => {
@@ -46,6 +54,9 @@ app.use((err, req, res, next) => {
 app.listen(process.env.PORT || 4000, () => {
     console.log("Hi Server Tasker")
 })
+
+
+
 
 
 
