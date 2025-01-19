437b6136236f97fb38594cf46a43ca03d86a19c9
hussin-hegazy <hussinhegazy350@gmail.com>
2025-01-19 02:59:44 +0200

    Updated User model to support Google login: added googleId field and made password optional

--- models/User.js
+++ models/User.js
@@ -6,9 +6,10 @@ const UserRole = require('../utils/UserRole')
 
 
 const user_schema= new mongoose.Schema({
-    email: {type: String,required: true, unique: true, validate: [validator.isEmail, "Faild be most a valid Email"]},
+    email: {type: String,required: true, unique: true, validate: [validator.isEmail, "Faild be most a valid Email"], index: true},
     name:  {type: String, required: true},
-    password: {type: String, required: true},
+    googleId: {type: String, unique: true, sparse: true},
+    password: {type: String, required: function() {return !this.googleId} },
     createdAt: {type: Date,  default: Date.now},
     token: {type: String},
     role: {type: String, enum: [UserRole.USER,UserRole.ADMIN,UserRole.MANAGER], default: 'USER'},
@@ -17,11 +18,5 @@ const user_schema= new mongoose.Schema({
 })
 
 
-// user_schema.pre("save", async function (next)  {
-//     if(this.isModified('password')){
-//         this.password = await bcrypt.hash(this.password, 10)
-//     }
-//     next()
-// })
 
 module.exports = mongoose.model("User", user_schema)
